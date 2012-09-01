97670aa9618c9a96aabf35de34bc08b5040229fb
Chris Bentivenga <chris.bentivenga@me.com>
2012-08-31 23:43:44 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Qdb2::Application.config.secret_token = '12d3bcac837c7541d65350e8d742ed2a40f581fab615808873ec63654d06f4563ce9aabf9255ab7a70201cefb2650efca333d0577bf7cb6cc59d1fa4fbd30206'
