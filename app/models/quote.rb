be5d43bb5aef52234a53a31d19accf0dbcb1443a
Chris Bentivenga <chris.bentivenga@me.com>
2012-09-11 15:21:41 -0400

    Changed my midn, I want to sort by id lolz

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,5 +2,5 @@ class Quote < ActiveRecord::Base
   attr_accessible :approved, :body, :deleted, :description, :flagged
   has_and_belongs_to_many :tags
 
-  default_scope :order => "created_at DESC"
+  default_scope :order => "id DESC"
 end
