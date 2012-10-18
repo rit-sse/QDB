1105ab58732cedf663695d72c0c322379a0b043c
Chris Bentivenga <chris.bentivenga@me.com>
2012-10-18 11:38:53 -0400

    Fixed the tags json generation

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -3,4 +3,10 @@ class Quote < ActiveRecord::Base
   has_and_belongs_to_many :tags
 
   default_scope :order => "id DESC"
+
+  def as_json(options = { })
+    h = super(options)
+    h["tags"] = tags.map(&:name).join(",")
+    h
+  end
 end
